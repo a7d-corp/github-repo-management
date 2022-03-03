f199b3d75c30b9bc4f85864fd6b7d66f1b69676b
Simon Weald <glitchcrab-github@simonweald.com>
2022-03-03 22:32:33 +0000

    update tfstate file name

--- repositories/terraform-github-modules/_remote_state.tf
+++ repositories/terraform-github-modules/_remote_state.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket                      = "github-repo-management"
     force_path_style            = true
-    key                         = "terraform-module-github-repo.tfstate"
+    key                         = "terraform-github-modules.tfstate"
     skip_credentials_validation = true
     skip_metadata_api_check     = true
     skip_region_validation      = true
