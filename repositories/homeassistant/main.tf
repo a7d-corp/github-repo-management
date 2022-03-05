68e64f1c490405a1b63485aebe39285d7d3037e1
Simon Weald <7832341+glitchcrab@users.noreply.github.com>
2022-03-05 21:59:32 +0000

    add home assistant (#9)
    * add homeassistant repo
    
    * add atlantis config
    
    * add glitchcrab-bot as a collaborator

---
+++ repositories/homeassistant/main.tf
@@ -0,0 +1,22 @@
+module "github_repository" {
+  source = "github.com/glitchcrab/terraform-github-modules//github_repository"
+
+  repo_name        = var.repo_name
+  repo_description = var.repo_description
+  repo_visibility  = var.repo_visibility
+
+  topics = var.topics
+
+  has_issues = var.has_issues
+
+  allow_merge_commit     = var.allow_merge_commit
+  allow_rebase_merge     = var.allow_rebase_merge
+  delete_branch_on_merge = var.delete_branch_on_merge
+}
+
+module "github_repository_collaborator" {
+  source = "github.com/glitchcrab/terraform-github-modules//github_repository_collaborator"
+
+  repo_name         = var.repo_name
+  collaborator_name = var.collaborator_name
+}
---
+++ repositories/homeassistant/terraform.tfvars
@@ -0,0 +1,13 @@
+repo_name        = "homeassistant"
+repo_description = "My personal Home Assistant config"
+repo_visibility  = "public"
+
+topics = ["home-assistant", "home-automation", "home-assistant-config", "homeassistant"]
+
+has_issues = true
+
+allow_merge_commit     = false
+allow_rebase_merge     = false
+delete_branch_on_merge = true
+
+collaborator_name = "glitchcrab-bot"
