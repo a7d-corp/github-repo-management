3cfd3f23d913934da9afc733ae71a962a529d2c0
Simon Weald <7832341+glitchcrab@users.noreply.github.com>
2022-03-28 22:01:06 +0100

    add homelab-instance-template (#17)
    * add homelab-instance-template
    
    * set repo as a template

---
+++ repositories/homelab-instance-template/main.tf
@@ -0,0 +1,17 @@
+module "github_repository" {
+  source = "github.com/a7d-corp/terraform-github-modules//github_repository"
+
+  repo_name        = var.repo_name
+  repo_description = var.repo_description
+  repo_visibility  = var.repo_visibility
+
+  is_template = var.is_template
+
+  topics = var.topics
+
+  has_issues = var.has_issues
+
+  allow_merge_commit     = var.allow_merge_commit
+  allow_rebase_merge     = var.allow_rebase_merge
+  delete_branch_on_merge = var.delete_branch_on_merge
+}
---
+++ repositories/homelab-instance-template/terraform.tfvars
@@ -0,0 +1,15 @@
+repo_owner = "a7d-corp"
+
+repo_name        = "homelab-instance-template"
+repo_description = "Template repo for managing a Proxmox instance with Terraform."
+repo_visibility  = "public"
+
+is_template = true
+
+topics = ["proxmox", "virtualisation", "virtualization", "terraform", "homelab"]
+
+has_issues = true
+
+allow_merge_commit     = false
+allow_rebase_merge     = false
+delete_branch_on_merge = true
