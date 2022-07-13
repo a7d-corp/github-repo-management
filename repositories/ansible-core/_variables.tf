31d49fb7cab2c52e21c1c79a97e221a74b917ce7
Simon Weald <7832341+glitchcrab@users.noreply.github.com>
2022-07-13 21:56:22 +0100

    add ansible core repo (#34)

---
+++ repositories/ansible-core/_variables.tf
@@ -0,0 +1,37 @@
+# common vars
+
+variable "repo_owner" {
+  type = string
+}
+
+variable "repo_name" {
+  type = string
+}
+
+variable "repo_description" {
+  type = string
+}
+
+variable "repo_visibility" {
+  type = string
+}
+
+variable "has_issues" {
+  type = bool
+}
+
+variable "topics" {
+  type = list(any)
+}
+
+variable "allow_merge_commit" {
+  type = bool
+}
+
+variable "allow_rebase_merge" {
+  type = bool
+}
+
+variable "delete_branch_on_merge" {
+  type = bool
+}
---
+++ repositories/ansible-core/terraform.tfvars
@@ -0,0 +1,13 @@
+repo_owner = "a7d-corp"
+
+repo_name        = "ansible-core"
+repo_description = "Ansible core playbooks and configuration"
+repo_visibility  = "private"
+
+topics = ["ansible"]
+
+has_issues = true
+
+allow_merge_commit     = false
+allow_rebase_merge     = false
+delete_branch_on_merge = true
