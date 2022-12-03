587c894b341915ef75d88fa8d00d5d6fb167a565
Simon Weald <glitchcrab-github@simonweald.com>
2022-12-03 17:06:30 +0000

    add manifest and common repo files

---
+++ repositories/_common/github_repo_common_files.tf
@@ -0,0 +1,43 @@
+resource "github_repository_file" "codeowners" {
+  repository          = var.repo_name
+  branch              = "main"
+  file                = "CODEOWNERS"
+  content             = file("${path.module}/files/CODEOWNERS")
+  commit_message      = "Managed by Terraform"
+  commit_author       = "Terraform"
+  commit_email        = "terraform-github@simonweald.com"
+  overwrite_on_create = true
+}
+
+resource "github_repository_file" "workflow_create_release" {
+  repository          = var.repo_name
+  branch              = "main"
+  file                = ".github/workflows/zz_generated.create_release.yaml"
+  content             = file("${path.module}/files/workflows/zz_generated.create_release.yaml")
+  commit_message      = "Managed by Terraform"
+  commit_author       = "Terraform"
+  commit_email        = "terraform-github@simonweald.com"
+  overwrite_on_create = true
+}
+
+resource "github_repository_file" "workflow_create_release_pr" {
+  repository          = var.repo_name
+  branch              = "main"
+  file                = ".github/workflows/zz_generated.create_release_pr.yaml"
+  content             = file("${path.module}/files/workflows/zz_generated.create_release_pr.yaml")
+  commit_message      = "Managed by Terraform"
+  commit_author       = "Terraform"
+  commit_email        = "terraform-github@simonweald.com"
+  overwrite_on_create = true
+}
+
+resource "github_repository_file" "workflow_changelog_validate" {
+  repository          = var.repo_name
+  branch              = "main"
+  file                = ".github/workflows/changelog-validate.yaml"
+  content             = file("${path.module}/files/workflows/changelog-validate.yaml")
+  commit_message      = "Managed by Terraform"
+  commit_author       = "Terraform"
+  commit_email        = "terraform-github@simonweald.com"
+  overwrite_on_create = true
+}
