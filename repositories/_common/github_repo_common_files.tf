cc7f7e3b15a9b33b50bf57f399e41a0be1404acf
Simon Weald <7832341+glitchcrab@users.noreply.github.com>
2023-06-19 17:33:27 +0100

    add github action and config for terraform-docs for terraform module repos (#54)

--- repositories/_common/github_repo_common_files.tf
+++ repositories/_common/github_repo_common_files.tf
@@ -49,3 +49,33 @@ resource "github_repository_file" "workflow_changelog_validate" {
   commit_email        = "terraform-github@simonweald.com"
   overwrite_on_create = true
 }
+
+resource "github_repository_file" "workflow_terraform_docs_validate" {
+  depends_on = [module.github_repository]
+
+  count = var.is_terraform_module ? 1 : 0
+
+  repository          = var.repo_name
+  branch              = var.repo_branch
+  file                = ".github/workflows/terraform-docs-validate.yaml"
+  content             = file("${path.module}/files/workflows/terraform-docs-validate.yaml")
+  commit_message      = "Managed by Terraform"
+  commit_author       = "Terraform"
+  commit_email        = "terraform-github@simonweald.com"
+  overwrite_on_create = true
+}
+
+resource "github_repository_file" "config_terraform_docs" {
+  depends_on = [module.github_repository]
+
+  count = var.is_terraform_module ? 1 : 0
+
+  repository          = var.repo_name
+  branch              = var.repo_branch
+  file                = ".terraform-docs.yml"
+  content             = file("${path.module}/files/files/tooling/terraform-docs.yml")
+  commit_message      = "Managed by Terraform"
+  commit_author       = "Terraform"
+  commit_email        = "terraform-github@simonweald.com"
+  overwrite_on_create = true
+}
