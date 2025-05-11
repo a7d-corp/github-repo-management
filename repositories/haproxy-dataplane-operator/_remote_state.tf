ac2ff32f058f6642e250626b66e36435a07df455
Simon Weald <glitchcrab-github@simonweald.com>
2025-05-11 06:39:08 +0100

    update remote state config

--- repositories/haproxy-dataplane-operator/_remote_state.tf
+++ repositories/haproxy-dataplane-operator/_remote_state.tf
@@ -1,11 +1,11 @@
 terraform {
   backend "s3" {
     bucket                      = "github-repo-management"
-    force_path_style            = true
+    use_path_style              = true
     key                         = "haproxy-dataplane-operator.tfstate"
+    skip_requesting_account_id  = true
     skip_credentials_validation = true
     skip_metadata_api_check     = true
     skip_region_validation      = true
   }
 }
-
